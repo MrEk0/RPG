acd7a8001f2be9a4eea0a20ed7de39367878ac49
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-07-16 14:08:23 +0300

    Show level on the display

---
+++ RPG/Assets/Scripts/UI/XPLevelDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class XPLevelDisplay : MonoBehaviour
+{
+    Text xpLevel;
+    BaseStats baseStats;
+
+    private void Awake()
+    {
+        xpLevel = GetComponent<Text>();
+        baseStats = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xpLevel.text = String.Format("Level {0}", baseStats.GetLevel());
+    }
+}
